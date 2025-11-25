f4ef62a26026030c0f7abf91736a71a18176c00b
guilhermedosanjosilva77 <dosanjosguilherme88@gmail.com>
2025-11-24 22:48:38 -0300

    criação das pastas e iniciando pasta model

---
+++ src/model/Cliente.java
@@ -0,0 +1,31 @@
+package model;
+
+public class Cliente {
+    private Long ID;
+    private String nome;
+    private String telefone;
+
+    public Cliente() {
+    }
+
+    public Cliente(String nome, String telefone) {
+        this.nome = nome;
+        this.telefone = telefone;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getTelefone() {
+        return telefone;
+    }
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    
+
+}
